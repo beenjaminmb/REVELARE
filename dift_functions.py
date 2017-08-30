0a30b51b1166d88c074eadd8408aad06c804f093
amajest <amajest@cs.unm.edu>
2017-08-30 16:37:00 -0600

    Left in some print statements :|

--- dift_functions.py
+++ dift_functions.py
@@ -204,9 +204,6 @@ class DIFT():
         return rt
 
     def DIFT_taint_source(self, startAddress, elements):
-        print("TAINT SOURCE")
-        print(startAddress)
-        print(elements)
         for i in range (elements):
             self.taint[startAddress + i] = self.get_random_taint_vector()
             self.origtaint[startAddress + i] = self.taint[startAddress + i]
